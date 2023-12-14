d428f80491e23438d1deb3fc1cf9c1d2b7422adc
Harun Emrecan Karabağ <harunemrecankarabag@gmail.com>
2023-12-14 22:06:52 +0300

    Boxes' color was fixed. Game over panel was added. Enter function was added on text field.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,6 +8,7 @@ using UnityEngine.Windows;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 using Unity.VisualScripting;
+using System.Drawing;
 
 public class GameController : MonoBehaviour
 {
@@ -17,22 +18,53 @@ public class GameController : MonoBehaviour
     public String csvDosya = "imdb_top_250.csv";
     public String csvSplitBy = ";";
 
+    public UnityEngine.Color colour;
+    public UnityEngine.Color colour2;
+
     public TMP_InputField input;
 
-    public GameObject messageBox;
+    public GameObject messageBox, gameOverBox;
+    public TextMeshProUGUI gameover;
 
     public int correctAnswer;
 
     public int count;
+
+    public int counter;
+
+    private bool isSelected = false;
 
     private List<Film> filmListesi;
     private Film secilenFilm;
 
     void Start()
     {
+        string htmlColor = "#399e20";
+        string htmlColor2 = "#9e2020";
         filmListesi = FilmleriOku(csvDosya, csvSplitBy);
         secilenFilm = RandomFilmSelect(filmListesi);
         count = 0;
+        UnityEngine.ColorUtility.TryParseHtmlString(htmlColor, out colour);
+        UnityEngine.ColorUtility.TryParseHtmlString(htmlColor2, out colour2);
+    }
+
+    public void OnSelect()
+    {
+        isSelected = true;
+    }
+
+
+    public void OnDeselect()
+    {
+        isSelected = false;
+    }
+
+    private void Update()
+    {
+        if (UnityEngine.Input.GetKeyDown(KeyCode.Return) && isSelected)
+        {
+            CheckButton();
+        }
     }
 
     public void CheckButton()
@@ -57,6 +89,7 @@ public class GameController : MonoBehaviour
         }
         else
         {
+            counter = 0;
             Debug.LogError("Kullanýcý Film Bilgileri" + kullaniciFilm.filmAdi + "\n" + kullaniciFilm.tur + "\n" + kullaniciFilm.yil + "\n" + kullaniciFilm.yonetmen + "\n" + kullaniciFilm.ulke + "\n" + kullaniciFilm.yildiz);
             switch (count)
             {
@@ -64,77 +97,136 @@ public class GameController : MonoBehaviour
                     name0.SetActive(true);
                     tname0.text = kullaniciFilm.filmAdi;
                     if (string.Equals(kullaniciFilm.filmAdi, secilenFilm.filmAdi, StringComparison.OrdinalIgnoreCase))
-                    {
-                        Debug.Log("Girdi.");
-                        name0.GetComponent<SpriteRenderer>().color = new Color(0f, 1f, 0f);
-                    }
+                        name0.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        name0.GetComponent<SpriteRenderer>().color = colour2;
+
                     year0.SetActive(true);
                     tyear0.text = kullaniciFilm.yil;
                     if (kullaniciFilm.yil == secilenFilm.yil)
-                        year0.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        year0.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        year0.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     country0.SetActive(true);
                     tcountry0.text = kullaniciFilm.ulke;
                     if (kullaniciFilm.ulke == secilenFilm.ulke)
-                        country0.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        country0.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        country0.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     type0.SetActive(true);
                     ttype0.text = kullaniciFilm.tur;
                     if (kullaniciFilm.tur == secilenFilm.tur)
-                        type0.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        type0.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        type0.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     star0.SetActive(true);
                     tstar0.text = kullaniciFilm.yildiz;
                     if (kullaniciFilm.yildiz == secilenFilm.yildiz)
-                        star0.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        star0.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        star0.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     director0.SetActive(true);
                     tdirector0.text = kullaniciFilm.yonetmen;
                     if (kullaniciFilm.yonetmen == secilenFilm.yonetmen)
-                        director0.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        director0.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        director0.GetComponent<SpriteRenderer>().color = colour2;
                     count++;
                     break;
                 case 1:
                     name1.SetActive(true);
                     tname1.text = kullaniciFilm.filmAdi;
-                    if (kullaniciFilm.filmAdi == secilenFilm.filmAdi)
-                        name1.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                    if (string.Equals(kullaniciFilm.filmAdi, secilenFilm.filmAdi, StringComparison.OrdinalIgnoreCase))
+                        name1.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        name1.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     year1.SetActive(true);
                     tyear1.text = kullaniciFilm.yil;
                     if (kullaniciFilm.yil == secilenFilm.yil)
-                        year1.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        year1.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        year1.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     country1.SetActive(true);
                     tcountry1.text = kullaniciFilm.ulke;
                     if (kullaniciFilm.ulke == secilenFilm.ulke)
-                        country1.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        country1.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        country1.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     type1.SetActive(true);
+                    ttype1.text = kullaniciFilm.tur;
                     if (kullaniciFilm.tur == secilenFilm.tur)
-                        type1.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        type1.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        type1.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     star1.SetActive(true);
+                    tstar1.text = kullaniciFilm.yildiz;
                     if (kullaniciFilm.yildiz == secilenFilm.yildiz)
-                        star1.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        star1.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        star1.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     director1.SetActive(true);
+                    tdirector1.text = kullaniciFilm.yonetmen;
                     if (kullaniciFilm.yonetmen == secilenFilm.yonetmen)
-                        director1.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        director1.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        director1.GetComponent<SpriteRenderer>().color = colour2;
                     count++;
                     break;
                 case 2:
                     name2.SetActive(true);
                     tname2.text = kullaniciFilm.filmAdi;
-                    if (kullaniciFilm.filmAdi == secilenFilm.filmAdi)
-                        name2.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                    if (string.Equals(kullaniciFilm.filmAdi, secilenFilm.filmAdi, StringComparison.OrdinalIgnoreCase))
+                        name2.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        name2.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     year2.SetActive(true);
+                    tyear2.text = kullaniciFilm.yil;
                     if (kullaniciFilm.yil == secilenFilm.yil)
-                        year2.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        year2.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        year2.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     country2.SetActive(true);
+                    tcountry2.text = kullaniciFilm.ulke;
                     if (kullaniciFilm.ulke == secilenFilm.ulke)
-                        country2.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        country2.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        country2.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     type2.SetActive(true);
+                    ttype2.text = kullaniciFilm.tur;
                     if (kullaniciFilm.tur == secilenFilm.tur)
-                        type2.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        type2.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        type2.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     star2.SetActive(true);
+                    tstar2.text = kullaniciFilm.yildiz;
                     if (kullaniciFilm.yildiz == secilenFilm.yildiz)
-                        star2.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        star2.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        star2.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     director2.SetActive(true);
+                    tdirector2.text = kullaniciFilm.yonetmen;
                     if (kullaniciFilm.yonetmen == secilenFilm.yonetmen)
-                        director2.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0);
+                        director2.GetComponent<SpriteRenderer>().color = colour;
+                    else
+                        director2.GetComponent<SpriteRenderer>().color = colour2;
+                    
                     count++;
+                    gameOverBox.SetActive(true);
+                    gameover.text = "Kaybettiniz." + "\n" + "Doðru Cevap: " + secilenFilm.filmAdi;
                     break;
                 default:
                     Debug.Log("Countta Sýkýntý Var.");
@@ -144,6 +236,7 @@ public class GameController : MonoBehaviour
             Debug.Log("Kullanýcý Film: " + kullaniciFilm.getFilmAdi());
         }
     }
+
 
     private Film RandomFilmSelect(List<Film> filmListesi)
     {
